f3fb654529b2f5c55fc904c9e6f0e29e0dd56c9d
keijinanjo <satukaruma@gmail.com>
2018-10-11 17:03:53 +0900

    10/11　課題

--- Paiza/paiza-4-08-2.py
+++ Paiza/paiza-4-08-2.py
@@ -1,4 +1,4 @@
-# coding: utf-8
+﻿# coding: utf-8
 
 '''
 演習課題「おみくじを作ろう」
@@ -21,7 +21,14 @@
 # おみくじプログラム
 
 import random
-line = input().rstrip()
+line = input().rstrip().split(",")
+print(line)
+
+num = len(line)
+
+lottery = random.randrange(num)
+
+print(line[lottery])
 
 # 今回は自力で全部書いてみよう！
 
